e4614edb1e0d11ef5f3c7dbfc3feb10ab3f9344e
Kasper Helsted <fisseerskont@gmail.com>
2014-02-17 09:58:01 +0100

    sadf

--- Source/GroundHandling/GroundHandling/Main.cs
+++ Source/GroundHandling/GroundHandling/Main.cs
@@ -6,36 +6,10 @@ namespace GroundHandling
 	{
 		public static void Main (string[] args)
 		{
-<<<<<<< HEAD
-<<<<<<< HEAD
 			Console.WriteLine ("Hello Plane! \n");
             Console.WriteLine("We're flying....");
             Console.WriteLine("We're dieing....");
             Console.ReadLine();
         }
-=======
-=======
->>>>>>> aebcd7d0ecc7c189c8860fba649c5b286613c123
-            Console.WriteLine("And we say Ooohh, Ooohhh");
-            Console.WriteLine("OOh, We're flying....");
-		}
->>>>>>> ecaa8ea92a0987135d8ae764d1f33a6b8fc1d92a
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-//Christian lugter af sovs
-=======
-//Christian lugter af sovs
->>>>>>> aebcd7d0ecc7c189c8860fba649c5b286613c123
