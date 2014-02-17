ecaa8ea92a0987135d8ae764d1f33a6b8fc1d92a
amatt13 <anders.matthiassen@gmail.com>
2014-02-17 09:46:57 +0100

    Test test

--- Source/GroundHandling/GroundHandling/Main.cs
+++ Source/GroundHandling/GroundHandling/Main.cs
@@ -6,8 +6,21 @@ namespace GroundHandling
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello Plane! \n");
-			Console.WriteLine ("We're flying....");
+            Console.WriteLine("And we say Ooohh, Ooohhh");
+            Console.WriteLine("OOh, We're flying....");
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//Christian lugter af sovs
